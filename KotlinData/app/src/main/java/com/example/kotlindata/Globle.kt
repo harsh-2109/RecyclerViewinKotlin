f8006992534f3b08ddea9b858cd1cdd973ea5682
harsh-2109 <harshdpandya1995@gmail.com>
2020-04-04 22:37:55 -0400

    dataShowing with LiveData

--- KotlinData/app/src/main/java/com/example/kotlindata/Globle.kt
+++ KotlinData/app/src/main/java/com/example/kotlindata/Globle.kt
@@ -1,3 +1,5 @@
 package com.example.kotlindata
 
 const val LOG_VAL = "Monster_Log"
+
+const val WEB_SERVICE_URL = "https://774906.youcanlearnit.net"
--- KotlinData/app/src/main/java/com/example/kotlindata/data/MonsterRepository.kt
+++ KotlinData/app/src/main/java/com/example/kotlindata/data/MonsterRepository.kt
@@ -1,12 +1,17 @@
 package com.example.kotlindata.data
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.MutableLiveData
-import com.example.kotlindata.utilities.FileHelper
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
+import com.example.kotlindata.WEB_SERVICE_URL
 import com.squareup.moshi.Types
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MonsterRepository(private val application: Application) {
 
@@ -14,16 +19,42 @@ class MonsterRepository(private val application: Application) {
     private val listType = Types.newParameterizedType(List::class.java, Monster::class.java)
 
     init {
-        getMonsterData()
+//        getMonsterData()
+        CoroutineScope(Dispatchers.IO).launch {
+            callWebService()
+        }
+//        Log.i(LOG_VAL,"Network available: ${networkAvailable()}")
     }
 
-    fun getMonsterData() {
-        val text: String = FileHelper.getTextFromAssets(application, "monster_data.json")
+    @WorkerThread
+    suspend fun callWebService() {
+        if (networkAvailable()) {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(WEB_SERVICE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+            val service = retrofit.create(MonsterService::class.java)
+            val serviceData = service.getMonsterData().body() ?: emptyList()
+            monsterData.postValue(serviceData)
+        }
+    }
 
-        val moshi = Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-        val adapter: JsonAdapter<List<Monster>> = moshi.adapter(listType)
-        monsterData.value = adapter.fromJson(text) ?: emptyList()
+//    fun getMonsterData() {
+//        val text: String = FileHelper.getTextFromAssets(application, "monster_data.json")
+//
+//        val moshi = Moshi.Builder()
+//            .add(KotlinJsonAdapterFactory())
+//            .build()
+//        val adapter: JsonAdapter<List<Monster>> = moshi.adapter(listType)
+//        monsterData.value = adapter.fromJson(text) ?: emptyList()
+//    }
+
+    @Suppress("DEPRECATION")
+    fun networkAvailable(): Boolean {
+        val connectivityManager =
+            application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+
+        return networkInfo?.isConnectedOrConnecting ?: false
     }
 }
